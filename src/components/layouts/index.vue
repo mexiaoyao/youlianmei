667cc9fdba2d2d03c47509041d417488c21405db
zhengxiaolei <zhengxiaolei@vrvmail.com.cn>
2022-08-11 19:54:56 +0800

    ant-design-vue和layouts

---
+++ src/components/layouts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout>
+    <a-layout-header>
+      <router-link to="/">Home 头部</router-link>|
+      <router-link to="/about">About 头部</router-link>
+    </a-layout-header>
+    <a-layout-content>
+      <router-view />
+    </a-layout-content>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
+</template>
+<script>
+export default {
+  name: "layouts"
+};
+</script>
