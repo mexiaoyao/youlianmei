2ea12b87d9c6d08b79400304da86fc9fdd19676c
zhenglei <mexiaoyao@163.com>
2022-08-24 22:08:31 +0800

    假数据

--- src/views/finance/list/index.vue
+++ src/views/finance/list/index.vue
@@ -27,11 +27,14 @@
                 <a-table-column align="center" title="序号">
                     <template slot-scope="text, item, index">{{ index+1 }}</template>
                 </a-table-column>
-                <a-table-column align="center" data-index="date" title="日期" />
-                <a-table-column align="center" data-index="feild1" title="新增好友数" />
-                <a-table-column align="center" data-index="feild2" title="活跃好友数" />
-                <a-table-column align="center" data-index="feild3" title="解除好友数" />
-                <a-table-column align="center" data-index="feild4" title="拉黑好友数" />
+                <a-table-column align="center" data-index="indexType" title="指数类型" />
+                <a-table-column align="center" data-index="codeNumber" title="股票代码" />
+                <a-table-column align="center" data-index="sharesName" title="股票名称" />
+                <a-table-column align="center" data-index="sharesAlise" title="股票别名" />
+                <a-table-column align="center" data-index="status" title="状态" />
+                <a-table-column align="center" data-index="createTime" title="添加时间" />
+                <a-table-column align="center" data-index="updateTime" title="修改时间" />
+                <a-table-column align="center" data-index="remarks" title="备注" />
             </a-table>
         </a-row>
     </div>
@@ -49,7 +52,129 @@ export default {
                 code: "",
                 alias: "",
             },
-            tableData: [],
+            tableData: [
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+                {
+                    id:1,
+                    indexType:1,
+                    codeNumber:'002400',
+                    sharesName:'省广集团',
+                    sharesAlise:'广东省广集团',
+                    status:0,
+                    createTime:'2022.05.06 15:16:10',
+                    updateTime:'2022.05.06 15:16:10',
+                    remarks:''
+                },
+            ],
             pagination: {},
             locale: { emptyText: <a-empty description="暂无数据" /> },
         };
