b766b756bde9874245c0d666f64781836cf29249
zhengxiaolei <zhengxiaolei@vrvmail.com.cn>
2022-09-08 16:21:36 +0800

    时间组件汉化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+    <a-config-provider :locale="locale">
+        <div id="app">
+            <router-view />
+        </div>
+    </a-config-provider>
 </template>
-
+<script>
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+import moment from "moment";
+import "moment/locale/zh-cn";
+moment.locale("zh-cn");
+export default {
+    data() {
+        return {
+            locale: zhCN,
+        };
+    },
+};
+</script>
+>
 <style lang="less">
 </style>
