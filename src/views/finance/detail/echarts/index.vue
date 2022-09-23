bd2e49fccff94450cc14e4e02557979381e2c79f
zhengxiaolei <zhengxiaolei@vrvmail.com.cn>
2022-09-23 18:16:28 +0800

    echarts

--- src/views/finance/detail/echarts/index.vue
+++ src/views/finance/detail/echarts/index.vue
@@ -32,11 +32,12 @@
             </a-form>
         </a-row>
         <a-row>
-            <div id="echartsId"></div>
+            <div id="echartsId" style="width:100%; height:300px;"></div>
         </a-row>
     </div>
 </template>
 <script>
+import axios from "axios";
 export default {
     name: "finance-detail-echarts",
     data() {
@@ -55,14 +56,75 @@ export default {
             this.getfinanceDetail();
         },
         getfinanceDetail() {
-            let parme = Object.assign(this.form, timeRange);
-            axios.post("/ylm/finance/detail", parme).then((res) => {
+            axios.post("/ylm/finance/detail", {}).then((res) => {
                 this.list = res.data.data.list || [];
+                this.drawChart();
             });
         },
         drawChart() {
             let myChart = this.$echarts.init(document.getElementById("echartsId"));
-            let option = {};
+            let option = {
+                title: {
+                    text: "Stacked Line",
+                },
+                tooltip: {
+                    trigger: "axis",
+                },
+                legend: {
+                    data: ["Email", "Union Ads", "Video Ads", "Direct", "Search Engine"],
+                },
+                grid: {
+                    left: "3%",
+                    right: "4%",
+                    bottom: "3%",
+                    containLabel: true,
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {},
+                    },
+                },
+                xAxis: {
+                    type: "category",
+                    boundaryGap: false,
+                    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                },
+                yAxis: {
+                    type: "value",
+                },
+                series: [
+                    {
+                        name: "Email",
+                        type: "line",
+                        stack: "Total",
+                        data: [120, 132, 101, 134, 90, 230, 210],
+                    },
+                    {
+                        name: "Union Ads",
+                        type: "line",
+                        stack: "Total",
+                        data: [220, 182, 191, 234, 290, 330, 310],
+                    },
+                    {
+                        name: "Video Ads",
+                        type: "line",
+                        stack: "Total",
+                        data: [150, 232, 201, 154, 190, 330, 410],
+                    },
+                    {
+                        name: "Direct",
+                        type: "line",
+                        stack: "Total",
+                        data: [320, 332, 301, 334, 390, 330, 320],
+                    },
+                    {
+                        name: "Search Engine",
+                        type: "line",
+                        stack: "Total",
+                        data: [820, 932, 901, 934, 1290, 1330, 1320],
+                    },
+                ],
+            };
             myChart.setOption(option);
         },
     },
