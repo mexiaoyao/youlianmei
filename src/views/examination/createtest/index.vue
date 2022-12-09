eaf7192b67ee3ab13481caf0d67f843ec909454b
zhengxiaolei <zhengxiaolei@vrvmail.com.cn>
2022-12-09 17:19:54 +0800

    提交

--- src/views/examination/createtest/index.vue
+++ src/views/examination/createtest/index.vue
@@ -4,20 +4,26 @@
             <a-button @click="createTest()" type="primary">生成PDF试卷</a-button>
         </a-row>
         <a-row ref="pdf" style="color:#000;">
-            <a-row class="tc testH1">语文练习题</a-row>
+            <a-row class="tc testH1">{{testData.title}}练习题</a-row>
+            <a-row>一、{{testData.one.title}}</a-row>
+            <a-row></a-row>
+            <a-row>二、{{testData.two.title}}</a-row>
+            <a-row></a-row>
+            <a-row>三、{{testData.three.title}}</a-row>
+            <a-row></a-row>
 
             <a-row class="tr">生成日期:2022-12-07</a-row>
         </a-row>
+        <menu-modal />
     </div>
 </template>
 <script>
 import { downloadPDF } from "@/libs/utils/pdf.js";
 
-import { GradeDictControl, GradeQuestionControl } from "@/api";
-
+import MenuModal from "./modal/menu";
 export default {
     name: "grade-question-list",
-    computed: {},
+    components: { MenuModal },
     provide() {
         return {
             testData: this.testData,
@@ -25,10 +31,21 @@ export default {
     },
     data() {
         return {
-            testData: {},
+            testData: {
+                visible: false, //右侧浮动菜单是否显示
+                title: "",
+                one: {
+                    title: "",
+                },
+                two: {
+                    title: "",
+                },
+                three: {
+                    title: "",
+                },
+            },
         };
     },
-    watch: {},
     created() {},
     methods: {
         createTest() {
